9c278d25d31bf64a70de87c656d74c7415310796
smitchell2240 <swmitchell@gmail.com>
2015-06-09 13:43:57 -0600

    fixed typo in transfer servlet

--- src/main/java/com/bandwidth/sdk/xml/examples/TransferServlet.java
+++ src/main/java/com/bandwidth/sdk/xml/examples/TransferServlet.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class tTransferServlet extends HttpServlet {
+public class TransferServlet extends HttpServlet {
 	public static final Logger logger = Logger
 			.getLogger(TransferServlet.class.getName());
 
